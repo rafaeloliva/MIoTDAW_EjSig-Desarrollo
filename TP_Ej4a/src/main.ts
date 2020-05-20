d960b69f5d09017f7f4b89fc8a8e8c6942039554
Rafael Oliva <roliva@uarg.unpa.edu.ar>
2020-05-20 11:05:58 -0300

    Acceso b=elemento boton, modifico label boton,creamos metodo mostrarUsuarios
    Este metodo toma la iista usuarios y modifica el innerHTML de dicha lista

--- TP_Ej4a/src/main.ts
+++ TP_Ej4a/src/main.ts
@@ -1,61 +1,46 @@
+// Ej 4+4.2 R.Oliva IoT DAW 2020
 // Class User tomado de Ejemplo en Clase 3
-class User {
-    private _id:number;
-    private _name:string;
-    private _email:string;
-    private _isLogged:boolean;
-
-    constructor(id:number, email:string, name:string){
-        this.id = id;
-        this.email = email;
-        this.name = name;
-        this.isLogged = false;
-    }
-
-    set id(id:number){
-        this._id = id;
-    }
-    get id():number {
-        return this._id;
-    }
-    set email(email:string){
-        this._email = email;
-    }    
-    get email():string {
-        return this._email;
-    }
-    set name(name:string){
-        this._name = name;
-    }    
-    get name():string {
-        return this._name;
-    }
-    set isLogged(flag:boolean){
-        this._isLogged = flag;
-    }
-    get isLogged():boolean{
-        return this._isLogged;
-    }
-
-    public printInfo():void{
-        console.log("Nombre:"+this.name+" email:"+this.email+" logged:"+this.isLogged);
-    }
-
-}
+// En Ejs 4 se mueve a User.ts
 
 // Ej 3 modifica main para crear instancia usuarios de User
 class Main{
+   
+    myf:MyFramework;
+
     main():void{
-        console.log("TP Ej3 R.Oliva - IoT DAW Hola mundo");
+        
+        this.myf = new MyFramework();
+        console.log("TP Ej4 R.Oliva - IoT DAW Hola mundo");
+        let b:HTMLElement = this.myf.getElementById("boton");
+        console.log(b);
+  
+        b.textContent = "Haga click!";
+
         let usuarios:Array<User>;
         usuarios = new Array<User>();
         usuarios.push(new User(1,"jperez@perez.com","Juanito"));
         usuarios.push(new User(2,"hpepe@perez.com","Horacito"));
         usuarios.push(new User(3,"csosa@perez.com","Carlitos"));
 
-        for(let i in usuarios){
-            usuarios[i].printInfo();
+        // remplazamos salida por consola
+        this.mostrarUsuarios(usuarios);
+    }
+
+    mostrarUsuarios(users:Array<User>):void {
+        /*
+        let items:string="";
+        for(let i in users){
+          users[i].printInfo();
+          items+="<li>"+users[i].name+" "+users[i].email+"</li>";
         }
+        */
+        
+        let strTemplate:string=`${
+            users.map((item) =>`<li>${item.name} ${item.email}</li>`).join('')
+          }`;
+
+        let ul:HTMLElement = this.myf.getElementById("listaUsuarios");
+        ul.innerHTML = strTemplate;
     }
 }
 
