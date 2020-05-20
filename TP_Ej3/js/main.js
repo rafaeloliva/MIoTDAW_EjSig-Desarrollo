10186c47a1e341c70656c7a64ef7e34c745d4705
Rafael Oliva <roliva@uarg.unpa.edu.ar>
2020-05-20 10:10:34 -0300

    Copiamos TP2 hecho Clase 3 en TP3 - para agregar User class en main.ts

---
+++ TP_Ej3/js/main.js
@@ -0,0 +1,9 @@
+class Main {
+    main() {
+        console.log("TP Ej2 R.Oliva - IoT DAW Hola mundo (ii)");
+    }
+}
+window.onload = () => {
+    let m = new Main();
+    m.main();
+};
---
+++ TP_Ej3/src/main.ts
@@ -0,0 +1,13 @@
+class Main{
+    main():void{
+        console.log("TP Ej2 R.Oliva - IoT DAW Hola mundo (ii)");
+    }
+}
+
+window.onload = () => {
+    let m:Main = new Main();
+    m.main();
+}
+
+ 
+
